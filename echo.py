99af12b116cdef3bb01252ffa544243ed2e88f41
Balakrishnan Chandrasekaran <balakrishnan.c@gmail.com>
2020-09-02 10:10:21 +0200

    Finish implementation of dumb client and server

--- echo.py
+++ echo.py
@@ -7,40 +7,91 @@ import argparse
 import socket
 
 
+# Default port.
+DEF_PORT = 9988
+
+# Connection backlog size.
+BACKLOG = 5
+
+# Buffer size.
+BUF_SZ = 4096
+
+
 def mk_socket():
     """Make TCP socket."""
-    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
+    return s
 
 
-def run_client():
-    pass
+def run_client(target, port, num_pkts=10):
+    """Run TCP echo client."""
+    cli = mk_socket()
+
+    # TODO: Pass IP address rather than hard-code the default.
+    cli.connect((target, port))
+
+    data = ('*' * 512).encode('utf-8')
+
+    with cli:
+        for _ in range(num_pkts):
+            cli.send(data)
 
 
-def run_server(port):
+def handle_cli(sock, addr, verbose):
+    """Handle connection from client."""
+    data = sock.recv(BUF_SZ)
+    n = sock.send(data)
+
+    # Check if we echoed all the bytes back.
+    if n != len(data):
+        raise ValueError("Failed to echo all bytes!")
+
+
+def run_server(port, verbose=False):
     """Run TCP echo server."""
     srv = mk_socket()
-    # TODO: Pass interface.
+
+    # TODO: Pass interface rather than hard-code the default.
     srv.bind(('', port))
+    srv.listen(BACKLOG)
+
+    try:
+        with srv:
+            while True:
+                cli, addr = srv.accept()
+
+                if verbose:
+                    print(f"> received connection from {addr}")
+
+                # TODO: Spawn a thread to handle the client.
+                handle_cli(cli, addr, verbose)
+    except KeyboardInterrupt:
+        # Killed from terminal via CTRL-C
+        if verbose:
+            print("> quit.")
 
 
 def main(args):
     if args.target:
         # Run client.
-        pass
+        run_client(args.target, args.port, args.verbose)
     else:
         # Run server.
-        pass
+        run_server(args.port, args.verbose)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Run TCP echo server or client")
+    parser.add_argument('--verbose', '-v',
+                        action='store_true',
+                        help='Enable verbose output')
     parser.add_argument('--port', dest='port',
                         type=int,
-                        default=9988,
+                        default=DEF_PORT,
                         help='Port number')
     parser.add_argument('--target', dest='target', metavar='target-IP',
                         type=str,
-                        default='localhost',
                         help=('Target IP address for a client to connect'))
     main(parser.parse_args())
